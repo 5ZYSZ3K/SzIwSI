d5b1b66fcea461566990d85e431b500d1ff4c187
5ZYSZ3K <pawel.szymon.poreba@gmail.com>
2025-10-19 22:00:44 +0200

    create net and run it

--- main.py
+++ main.py
@@ -1,53 +1,192 @@
 import numpy as np
-from TorchModel import TorchModel
-import pandas as pd
+from zipfile import ZipFile
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import normalize, StandardScaler
-import torch.optim as optim
-import torch.nn as nn
+import os
+import pandas as pd
 import torch
+from torch.utils.data import DataLoader, Dataset
+import torch.nn as nn
+import torch.optim as optim
+import random
+import torch.nn.functional as F
+import argparse
+from sklearn import metrics
+from tqdm.notebook import tqdm
+import gc
+import shutil
 
-x, y = [], []
+FILE_PATH = './grasp-and-lift-eeg-detection'
+list_dir = os.listdir(FILE_PATH)
 
-TEST_SUBJECT_1_SERIES_9_PATH = './grasp-and-lift-eeg-detection/test/subj1_series9_data.csv'
-TRAIN_SUBJECT_1_SERIES_1_DATA_PATH = './grasp-and-lift-eeg-detection/train/subj1_series1_data.csv'
-TRAIN_SUBJECT_1_SERIES_1_EVENTS_PATH = './grasp-and-lift-eeg-detection/train/subj1_series1_data.csv'
+labels = ['HandStart', 'FirstDigitTouch', 'BothStartLoadPhase', 'LiftOff',
+       'Replace', 'BothReleased']
 
-x = pd.read_csv(TRAIN_SUBJECT_1_SERIES_1_DATA_PATH).iloc[:, 1:].values.astype(np.float32)
-y = pd.read_csv(TRAIN_SUBJECT_1_SERIES_1_EVENTS_PATH).iloc[:, 1:].values.astype(np.float32)
-x_train, x_test = train_test_split(x, test_size=0.2)
-y_train, y_test = train_test_split(y, test_size=0.2)
+torch.manual_seed(2021)
+np.random.seed(2021)
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-net = TorchModel()
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_epochs", type=int, default=1, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=1024, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.002, help="adam's learning rate")
+parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2", type=float, default=0.99, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--n_cpu", type=int, default=1, help="number of cpu threads to use during batch generation")
+parser.add_argument("--in_len", type=int, default=2**10, help="length of the input fed to neural net")
+parser.add_argument("--in_channels", type=int, default=32, help="number of signal channels")
+parser.add_argument("--out_channels", type=int, default=6, help="number of classes")
+parser.add_argument("--chunk", type=int, default=1000, help="length of splited chunks")
+opt, unknown = parser.parse_known_args()
+print(device)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-batch_size = 4
-trainloader = torch.utils.data.DataLoader(x_train, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)
-testloader = torch.utils.data.DataLoader(x_test, batch_size=batch_size,
-                                         shuffle=False, num_workers=2)
-for epoch in range(2):
+def read_csv(data, events):
+    x = pd.read_csv(data)
+    y = pd.read_csv(events)
+    id = '_'.join(x.iloc[0, 0].split('_')[:-1])
+    x = x.iloc[:,1:].values
+    y = y.iloc[:,1:].values
+    return x, y
+    
+trainset = []
+gt = []
+for filename in os.listdir(f'./{FILE_PATH}/train'):
+    if 'data' in filename:
+        data_file_name = os.path.join(f'./{FILE_PATH}/train', filename)
+        id = filename.split('.')[0]
+        events_file_name = os.path.join(f'./{FILE_PATH}/train', '_'.join(id.split('_')[:-1]) + '_events.csv')
+        x, y = read_csv(data_file_name, events_file_name)
+        trainset.append(x.T.astype(np.float32))
+        gt.append(y.T.astype(np.float32))
 
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # pobranie danych
-        inputs, labels = data
+valid_dataset = trainset[-1:]
+valid_gt = gt[-1:]
+trainset = trainset[:-1]
+gt = gt[:-1]
 
-        # ustawienie wszystkich optymalizowanych gradientow na zero
-        optimizer.zero_grad()
+all_data = np.concatenate(trainset, axis=1)  # trainset: [(32, N1), (32, N2), ...]
+m = np.mean(all_data, axis=1, keepdims=True)
+s = np.std(all_data, axis=1, keepdims=True)
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
+trainset = [(data - m) / s for data in trainset]
+
+valid_dataset = [(data - m) / s for data in valid_dataset]
+
+def resample_data(gt, chunk_size=opt.chunk):
+    """
+    split long signals to smaller chunks, discard no-events chunks  
+    """
+    total_discard_chunks = 0
+    mean_val = []
+    threshold = 0.01
+    index = []
+    
+    for i in range(len(gt)):
+        for j in range(0, gt[i].shape[1], chunk_size):
+            mean_val.append(np.mean(gt[i][:, j:min(gt[i].shape[1],j+chunk_size)]))
+            if mean_val[-1] < threshold:
+                total_discard_chunks += 1
+            else:
+                index.extend([(i, k) for k in range(j, min(gt[i].shape[1],j+chunk_size))])
+
+    # plt.plot([0, len(mean_val)], [threshold, threshold], color='r')
+    # plt.scatter(range(len(mean_val)), mean_val, s=1)
+    # plt.show()
+    print('Total number of chunks discarded: {} chunks'.format(total_discard_chunks))
+    print('{}% data'.format(total_discard_chunks/len(mean_val)))
+    del mean_val
+    gc.collect()
+    return index
+
+class EEGSignalDataset(Dataset):
+    def __init__(self, data, gt, m=m, s=s, soft_label=True, train=True):
+        self.data = data
+        self.gt = gt
+        self.train = train
+        self.soft_label = soft_label
+        self.eps = 1e-7
+        if train:
+            self.index = resample_data(gt)
+        else:
+            self.index = [(i, j) for i in range(len(data)) for j in range(data[i].shape[1])]
+        for dt in self.data:
+            dt -= m
+            dt /= s + self.eps
+    
+    def __getitem__(self, i):
+        i, j = self.index[i]
+        raw_data, label = self.data[i][:, max(0, j - opt.in_len + 1):j + 1], self.gt[i][:, j]
+
+        pad = opt.in_len - raw_data.shape[1]
+        if pad:
+            raw_data = np.pad(raw_data, ((0, 0), (pad, 0)), 'constant', constant_values=0)
+
+        raw_data = torch.from_numpy(raw_data.astype(np.float32))
+        label = torch.from_numpy(label.astype(np.float32))
+
+        if self.soft_label:
+            label[label < .02] = .02
+
+        return raw_data, label
+
+    def __len__(self):
+        return len(self.index)
+
+dataset = EEGSignalDataset(trainset, gt) 
+dataloader = DataLoader(dataset, batch_size=opt.batch_size, shuffle=True)
+print(len(dataset))
+
+class NNet(nn.Module):
+    def __init__(self, in_channels=opt.in_channels, out_channels=opt.out_channels):
+        super(NNet, self).__init__()
+        self.hidden = 32
+        self.net = nn.Sequential(
+            nn.Conv1d(opt.in_channels, opt.in_channels, 5, padding=2),
+            nn.Conv1d(self.hidden, self.hidden, 16, stride=16),
+            nn.LeakyReLU(0.1),
+            nn.Conv1d(self.hidden, self.hidden, 7, padding=3),
+        )
+        for i in range(6):
+            self.net.add_module('conv{}'.format(i), \
+                                self.__block(self.hidden, self.hidden))
+        self.net.add_module('final', nn.Sequential(
+            nn.Conv1d(self.hidden, out_channels, 1),
+            nn.Sigmoid()
+        ))
+        
+    def __block(self, inchannels, outchannels):
+        return nn.Sequential(
+            nn.MaxPool1d(2, 2),
+            nn.Dropout(p=0.1, inplace=True),
+            nn.Conv1d(inchannels, outchannels, 5, padding=2),
+            nn.LeakyReLU(0.1),
+            nn.Conv1d(outchannels, outchannels, 5, padding=2),
+            nn.LeakyReLU(0.1),
+        )
+    
+    def forward(self, x):
+        return self.net(x)
+    
+nnet = NNet()
+nnet.to(device)
+loss_fnc = nn.BCELoss()
+adam = optim.Adam(nnet.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+loss_his, train_loss = [], []
+nnet.train()
+for epoch in range(opt.n_epochs):
+    p_bar = dataloader
+    print(len(p_bar))
+    for i, (x, y) in enumerate(p_bar):
+        x, y = x.to(device), y.to(device)
+        pred = nnet(x)
+        loss = loss_fnc(pred.squeeze(dim=-1), y)
+        adam.zero_grad()
         loss.backward()
-        optimizer.step()
-
-        # wydrukowanie statystyk
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # wydrukowanie wynikow co 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
-
-print('Trening ukończony')
+        adam.step()
+        train_loss.append(loss.item())
+        # p_bar.set_description('[Loss: {}]'.format(train_loss[-1]))
+        if i % 50 == 0:
+            loss_his.append(np.mean(train_loss))
+            train_loss.clear()
+    print('[Epoch {}/{}] [Loss: {}]'.format(epoch+1, opt.n_epochs, loss_his[-1]))
+    
+torch.save(nnet.state_dict(), 'model.pt')
