f842d893a5293907d65af162d5a2ee18286e9deb
natruszka <ola.michalska2002@gmail.com>
2025-10-26 14:43:31 +0100

    Add docs

--- main.py
+++ main.py
@@ -10,14 +10,10 @@ import gc
 FILE_PATH = './grasp-and-lift-eeg-detection'
 list_dir = os.listdir(FILE_PATH)
 
-labels = ['HandStart', 'FirstDigitTouch', 'BothStartLoadPhase', 'LiftOff',
-       'Replace', 'BothReleased']
 
-torch.manual_seed(2021)
-np.random.seed(2021)
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-
-print(device)
+# this is to ensure reproductability of our code. 
+torch.manual_seed(2025)
+np.random.seed(2025)
 
 def read_csv(data, events):
     x = pd.read_csv(data)
@@ -33,29 +29,28 @@ for filename in os.listdir(f'./{FILE_PATH}/train'):
         data_file_name = os.path.join(f'./{FILE_PATH}/train', filename)
         id = filename.split('.')[0]
         events_file_name = os.path.join(f'./{FILE_PATH}/train', '_'.join(id.split('_')[:-1]) + '_events.csv')
-        x, y = read_csv(data_file_name, events_file_name)
-        trainset.append(x.T.astype(np.float32))
-        gt.append(y.T.astype(np.float32))
+        x_train, y_train = read_csv(data_file_name, events_file_name)
+        trainset.append(x_train.T.astype(np.float32))
+        gt.append(y_train.T.astype(np.float32))
 
 valid_dataset = trainset[-1:]
 valid_gt = gt[-1:]
 trainset = trainset[:-1]
 gt = gt[:-1]
 
+# normalize data
 all_data = np.concatenate(trainset, axis=1)  # trainset: [(32, N1), (32, N2), ...]
 m = np.mean(all_data, axis=1, keepdims=True)
 s = np.std(all_data, axis=1, keepdims=True)
-
 trainset = [(data - m) / s for data in trainset]
-
 valid_dataset = [(data - m) / s for data in valid_dataset]
 
 chunk_size = 1000
 
-def resample_data(gt):
-    """
-    split long signals to smaller chunks, discard no-events chunks  
-    """
+# split long signals to smaller chunks, discard no-events chunks 
+# if chunk's mean is lower than threshold we discard it, because there was no activity during that time
+# we keep a record of indices that remained in training dataset
+def clean_and_resample_data(gt):
     total_discard_chunks = 0
     mean_val = []
     threshold = 0.01
@@ -78,6 +73,7 @@ def resample_data(gt):
     gc.collect()
     return index
 
+
 class EEGSignalDataset(Dataset):
     def __init__(self, data, gt, m=m, s=s, soft_label=True, train=True):
         self.data = data
@@ -86,13 +82,16 @@ class EEGSignalDataset(Dataset):
         self.soft_label = soft_label
         self.eps = 1e-7
         if train:
-            self.index = resample_data(gt)
+            self.index = clean_and_resample_data(gt)
         else:
             self.index = [(i, j) for i in range(len(data)) for j in range(data[i].shape[1])]
         for dt in self.data:
             dt -= m
-            dt /= s + self.eps
+            dt /= s + self.eps # TODO it seems like data is normalized again here? should we remove it?
     
+    # get chunk of signal, if chunk is smaller than 1024 we add 0 padding
+    # if soft_label we avoid using exact zeros - we use 0.2 instead
+    # this is for loss functions with logarithms
     def __getitem__(self, i):
         i, j = self.index[i]
         raw_data, label = self.data[i][:, max(0, j - 1024 + 1):j + 1], self.gt[i][:, j]
@@ -112,10 +111,11 @@ class EEGSignalDataset(Dataset):
     def __len__(self):
         return len(self.index)
 
-dataset = EEGSignalDataset(trainset, gt) 
+dataset = EEGSignalDataset(trainset, gt) # TODO run it with soft_labels and check if it's better
 dataloader = DataLoader(dataset, batch_size=1024, shuffle=True)
 print(len(dataset))
 
+# we return 6 channels because our dataset specify 6 hand gestures
 class NNet(nn.Module):
     def __init__(self, in_channels=32, out_channels=6):
         super(NNet, self).__init__()
@@ -126,12 +126,12 @@ class NNet(nn.Module):
             nn.LeakyReLU(0.1),
             nn.Conv1d(self.hidden, self.hidden, 7, padding=3),
         )
-        for i in range(6):
+        for i in range(6): # six different hand gestures
             self.net.add_module('conv{}'.format(i), \
                                 self.__block(self.hidden, self.hidden))
         self.net.add_module('final', nn.Sequential(
             nn.Conv1d(self.hidden, out_channels, 1),
-            nn.Sigmoid()
+            nn.Sigmoid() # produces 1 or 0, depending on input
         ))
         
     def __block(self, inchannels, outchannels):
@@ -141,7 +141,7 @@ class NNet(nn.Module):
             nn.Conv1d(inchannels, outchannels, 5, padding=2),
             nn.LeakyReLU(0.1),
             nn.Conv1d(outchannels, outchannels, 5, padding=2),
-            nn.LeakyReLU(0.1),
+            nn.LeakyReLU(0.1), # TODO maybe we should try PReLU? - The learnable slope adjusts over time, which can yield gradients that are closer to optimal, especially in deeper networks.
         )
     
     def forward(self, x):
@@ -149,27 +149,23 @@ class NNet(nn.Module):
     
 n_epochs = 10
     
-nnet = NNet()
-nnet.to(device)
-loss_fnc = nn.BCELoss()
-adam = optim.Adam(nnet.parameters(), lr=0.002, betas=(0.5, 0.99))
-loss_his, train_loss = [], []
-nnet.train()
+model = NNet()
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters())
+loss_history, train_loss = [], []
+model.train()
+
 for epoch in range(n_epochs):
-    p_bar = dataloader
-    print(len(p_bar))
-    for i, (x, y) in enumerate(p_bar):
-        x, y = x.to(device), y.to(device)
-        pred = nnet(x)
-        loss = loss_fnc(pred.squeeze(dim=-1), y)
-        adam.zero_grad()
+    for i, (x_train, y_train) in enumerate(dataloader):
+        predicted_y = model(x_train)
+        loss = loss_function(predicted_y.squeeze(dim=-1), y_train)
+        optimizer.zero_grad()
         loss.backward()
-        adam.step()
+        optimizer.step()
         train_loss.append(loss.item())
-        # p_bar.set_description('[Loss: {}]'.format(train_loss[-1]))
         if i % 50 == 0:
-            loss_his.append(np.mean(train_loss))
+            loss_history.append(np.mean(train_loss))
             train_loss.clear()
-    print('[Epoch {}/{}] [Loss: {}]'.format(epoch+1, n_epochs, loss_his[-1]))
+    print('[Epoch {}/{}] [Loss: {}]'.format(epoch+1, n_epochs, loss_history[-1]))
     
-torch.save(nnet.state_dict(), 'model.pt')
+torch.save(model.state_dict(), 'model.pt')
